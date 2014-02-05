93e14720bf733a07ba3165fe03bb5bc1934452ee
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-02-05 09:45:12 +0100

    Added simple AsyncTask to use as Arduino serial receiver

---
+++ adktoolkit/src/main/java/me/palazzetti/adktoolkit/AdkReceiver.java
@@ -0,0 +1,19 @@
+package me.palazzetti.adktoolkit;
+
+import android.os.AsyncTask;
+
+/**
+ * AsyncTask to listen ADK serial communication port
+ */
+public abstract class AdkReceiver extends AsyncTask<AdkManager, String, Void> {
+
+    @Override
+    protected Void doInBackground(AdkManager... params) {
+        AdkManager adkManager = params[0];
+
+        while (adkManager.serialAvailable()) {
+            publishProgress(adkManager.readText());
+        }
+        return null;
+    }
+}
